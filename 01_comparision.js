750b825bedf5ce899115d526f6746509fe7171d0
Abhishek Kirar <39722476+imkirar@users.noreply.github.com>
2024-07-24 12:15:19 +0000

    type check

---
+++ 01_comparision.js
@@ -0,0 +1,34 @@
+// primitive datatypes
+// 7 type datatype
+
+/*
+7 type of primitive datatypes
+    String 
+    Symbol 
+    BigInt 
+    null 
+    String 
+    Number
+    Boolean
+*/ 
+
+/*
+ reference ? type of non primitive datatypes
+    
+    Array
+    Object
+    Functions
+
+*/ 
+
+
+const myAbhishek = function()
+{
+    console.log("hello world");
+}
+
+myAbhishek();
+
+console.log(typeof myAbhishek);
+// this is called function object
+
