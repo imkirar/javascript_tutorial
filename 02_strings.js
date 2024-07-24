e8eaf4185980ca02f8e8a3eb018aaf0330a67fdb
Abhishek Kirar <39722476+imkirar@users.noreply.github.com>
2024-07-24 12:49:53 +0000

    string check

---
+++ 02_strings.js
@@ -0,0 +1,11 @@
+const myName = "Abhishek"
+console.log(myName);
+
+const mychars = new String(myName)
+console.log(mychars);
+
+const mychars2 = new String("Hemant")
+console.log(mychars2);
+console.log(mychars2.length);
+console.log(mychars2.__proto__);
+console.log(mychars2.toUpperCase());
