b47a704750ae6459beb760a55ab4cfa1dcf228ba
Abhishek Kirar <39722476+imkirar@users.noreply.github.com>
2024-07-26 17:27:00 +0000

    edit two

---
+++ 08_loop.js
@@ -0,0 +1,27 @@
+const array = ['mango', 'apple', 'banana'];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i]
+//     console.log(element)
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("outer loop", i)
+//     for (let j = 0; j < 2; j++) {
+//         console.log("inner loop", j)
+        
+//     }
+// }
+
+// let index = 0;
+// while (index < array.length) {
+//     console.log(`value of index ${array[index]}`);
+//     index = index+1;
+// }
+
+
+
+// let count = 11
+// do {
+//     console.log(`score value is ${count}`);
+//     count++
+// } while (count <= 10);
